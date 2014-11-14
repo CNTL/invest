d5fc0d002732fc7ba8343e70213f8b193a5511a5
leijuan1014 <leijuan1014@163.com>
2014-11-14 21:26:32 +0800

    公共方法提交

--- java/common/src/com/tl/common/UploadHelper.java
+++ java/common/src/com/tl/common/UploadHelper.java
@@ -20,6 +20,7 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+
 /**
  * 文件上传辅助类
  * @author Gong Lijie
