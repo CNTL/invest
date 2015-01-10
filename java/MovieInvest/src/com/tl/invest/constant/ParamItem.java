fed899c1e2aad68d176518cdf1192326cb254578
rengy <rgy@outlook.com>
2015-01-11 00:02:11 +0800

    ALIPAY

--- java/MovieInvest/src/com/tl/invest/constant/ParamItem.java
+++ java/MovieInvest/src/com/tl/invest/constant/ParamItem.java
@@ -4,6 +4,9 @@ public class ParamItem {
 	public static final String LOGIN_QQ_URL = "QQ-URL";
 	public static final String LOGIN_QQ_AUTHORIZATIONKEY = "QQ-AuthorizationKey";
 	
-	public static final String PAY_ALI_URL = "ALI-URL";
-	public static final String PAY_ALI_AUTHORIZATIONKEY = "ALI-AuthorizationKey";
+	public static final String PAY_ALI_PARTNER = "ALI_商户身份ID";
+	public static final String PAY_ALI_KEY = "ALI_商户私钥";
+	public static final String PAY_ALI_NOTIFY_URL = "ALI_异步通知页面路径";
+	public static final String PAY_ALI_RETRUN_URL = "ALI_同步通知页面路径";
+	public static final String PAY_ALI_SELLER_EMAIL = "ALI_商户支付宝邮箱";
 }
